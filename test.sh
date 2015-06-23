21bb1da7e661349f3134631745d3b221dc9407b8
Zane McCaig <zane@instanews.com>
2015-06-23 18:03:03 -0400

    Improved the test cases a lot but they are still not perfect yet

--- test.sh
+++ test.sh
@@ -1,41 +1,64 @@
-export TUTUM_USER=zanemcca
-export TUTUM_PASS=@1Ph@c3p0f!sh
+TUTUM_USER=zanemcca
+TUTUM_PASS="@1Ph@c3p0f!sh"
 
+RANDOM=$$
+RAND=$RANDOM
+STACKNAME=$RAND-testStack
+LOADNAME=$RAND-load
+APPNAME=$RAND-app
+URL="http://`echo $LOADNAME`.`echo $STACKNAME`.`echo $TUTUM_USER`.svc.tutum.io"
+echo $URL
+
+##################################################################################
+# Functions 
+##################################################################################
+
+# Clean up function
 function quit {
+  kill -15 $ZERO_PID
   # Clean up the stack
-  tutum stack terminate --sync testStack
+  tutum stack terminate $STACKNAME 
   exit $1
 }
 
-zeroDone=1 
-# End the tryZeroDown call
-function zeroDone {
-  zeronDone=0
+# Catch signals and run cleanup function
+trap quit SIGHUP SIGINT SIGTERM
+
+# Get the httpCode from the URL
+function getHttpCode {
+  return `curl --retry 10 --retry-delay 5 -sL -w "%{http_code}\\n" $URL -o /dev/null`
 }
 
 # Continuosly curl the load balancer and never expect anything but 200
 function tryZeroDown {
-  failed=0
-  count=0
-  while $zeroDone > 0] 
+
+  local failed=0
+  local count=0 
+
+  trap "zeroClean \$failed \$count" SIGHUP SIGINT SIGTERM
+
+  while : 
   do
-    count = $count + 1
-    if [ curl --retry 10 --retry-delay 5 -sL -w "%{http_code}\\n" \
-      "http://load.testStack.`echo $TUTUM_USER`.svc.tutum.io" -o /dev/null \
-      -ne 200 ]
+    count=$(($count + 1))
+    if getHttpCode != 200
     then
-      failed = $failed + 1 
+      failed=$(($failed + 1)) 
     fi
     sleep 1
   done
-  if [ $failed > 0 ]
+}
+
+# Clean up function for the tryZeroDown function
+# zeroClean $failed $count
+function zeroClean {
+  if (($1 > 0));
   then
-    echo "FAILED: There were $failed non '200' response codes recieved of $count total calls"
-    quit -1
+    echo "FAILED: There were $1 non '200' response codes recieved of $2 total calls"
+    exit -1
   else
-    echo zero down!
+    echo "Success: All $2 requests returned 200 as expected" 
+    exit
   fi
-  zeroDone=1 
 }
 
 # Try will send a curl request to the load balancer and then verify that the proper
@@ -44,72 +67,122 @@ function tryZeroDown {
 # Where n is the number of active instances that are to be balanced across
 function try {
 
-  sleep 30 
-  rm output
-  # Perform $1 curl requests and log the output
-  for i in `seq 1 $((10 * $1))`;
+  local prev=0
+  local delay=1
+
+  local failed=0
+
+  while (($delay < 55));
   do
-    curl --retry 10 --retry-delay 5 -sSfL http://load.testStack.`echo $TUTUM_USER`.svc.tutum.io >> output 
+    rm -f output
+    # Perform $1 curl requests and log the output
+    for i in `seq 1 $((10 * $1))`;
+    do
+      curl --retry 10 --retry-delay 5 -sSfL $URL >> output
+    done
+
+    failed=0
+    # Check that $1 app instances were successfully load balanced across
+    for i in `seq 1 $1`;
+    do
+      if ! grep -q "My hostname is .*-app-$i" output;
+      then
+        failed=$i
+        break
+      fi
+    done
+
+    # If the test failed then increase the delay and sleep
+    if (($failed > 0));
+    then
+      echo "Trying again in $delay seconds"
+      sleep $delay
+      delay=$(($delay + $prev))
+      prev=$(($delay - $prev))
+    else
+        echo "Success: All $1 apps were called!"
+        break
+    fi
+
   done
 
-  # Check that $1 app instances were successfully load balanced across
-  for i in `seq 1 $1`;
-  do
-    if grep -Fq "My hostname is app-$i"  output
-    then
-      echo "Success: app-$i was called!"
-    else
-      echo "FAILED: app-$i was not called!"
-      quit -1 
-    fi
-  done
+  # Check if the test has failed
+  if (($failed > 0));
+  then
+    echo "FAILED: app-$failed was not called!"
+    quit -1
+  fi
 }
 
+##################################################################################
+# Main
+##################################################################################
+
 # Setup the image and login to ccounts/login/tutum
-tutum login -u `echo $TUTUM_USER`  -p `echo $TUTUM_PASS` -e test@instanews.com 
-docker build -t iload .
-tutum image push iload
+tutum login -u $TUTUM_USER  -p $TUTUM_PASS -e zane@instanews.com 
+#docker build -t iload .
+#tutum image push iload
+
+sed "s/lb/$LOADNAME/g" tutum.yml > custom.yml
+sed -i "s/app/$APPNAME/g" custom.yml
+sed -i "s/username/$TUTUM_USER/g" custom.yml
 
 # Start the stack and wait for it to be running before continuing
-tutum stack up --sync -n testStack -f tutum.yml
+tutum stack up --sync -n $STACKNAME -f custom.yml
+
+#rm -f custom.yml
+
+# Start services
 #tutum service run -t 2 --name webTest tutum/hello-world
 #tutum service run --role global --link webTest:webTest --name iloadTest tutum.co/`echo $TUTUM_USER`/iload 
 
 echo "===> Testing that all 3 instances of the app are being used"
 try 3
 
+echo "===> Testing zero-downtime redeployment"
+
+tryZeroDown &
+ZERO_PID=$!
+echo "======> Load Balancer reload"
+tutum service redeploy --sync $LOADNAME 
+kill -15 $ZERO_PID 
+sleep 1
+
+tryZeroDown &
+ZERO_PID=$!
+echo "======> App service reload"
+tutum service redeploy --sync $APPNAME 
+kill -15 $ZERO_PID
+sleep 1
+
+tryZeroDown &
+ZERO_PID=$!
+echo "======> Reload the whole stack"
+tutum stack redeploy --sync $STACKNAME 
+kill -15 $ZERO_PID
+sleep 1
+
 echo "===> Testing scaling up command on the app service"
-tutum service scale --sync app 4
+tutum service scale --sync $APPNAME 4
 try 4
 
 echo "===> Testing scaling down command on the app service"
-tutum service scale --sync app 3 
+echo "========= TODO check for lack of existence of app-4 ==="
+tutum service scale --sync $APPNAME 3 
 try 3
 
 echo "===> Testing stopping a container of the app service"
-tutum container stop --sync app-3
+echo "========= TODO check for lack of existence of app-3 ==="
+tutum container stop --sync "$APPNAME-3"
 try 2
 
 echo "===> Testing starting a container of the app service"
-tutum container start --sync app-3
+tutum container start --sync "$APPNAME-3"
 try 3 
 
 echo "===> Testing terminating a container of the app service"
-tutum container terminate --sync app-3
+echo "========= TODO check for lack of existence of app-3 ==="
+tutum container terminate --sync "$APPNAME-3"
 try 2
-
-echo "===> Testing zero-downtime redeployment"
-tryZeroDown &
-
-echo "======> Load Balancer reload"
-tutum service redeploy --sync load
-
-echo "======> App service reload"
-tutum service redeploy --sync app 
-
-echo "======> Reload the whole stack"
-tutum stack redeploy --sync testStack 
-
-zeroDone
 
 quit
